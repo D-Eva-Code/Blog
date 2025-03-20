33015aaf16b43d6159636e42d9358340433f950e
Eva <anagwonyeadaeze42@gmail.com>
2025-03-20 21:01:51 +0100

    linking smtp with views

--- Blog/Blog_app/urls.py
+++ Blog/Blog_app/urls.py
@@ -4,8 +4,10 @@ from Blog_app import views
 
 
 urlpatterns = [
-    path('list', views.Postlistview.as_view(), name = 'list'),
-    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name= 'postdetail')
+    # path('list', views.Postlistview.as_view(), name = 'list'),
+    path('list', views.post_list, name= 'list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name= 'postdetail'),
+    path('<int:id>/share/', views.post_share, name= 'postshare'),
 ]
 
  
--- Blog/Blog_app/views.py
+++ Blog/Blog_app/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Post
-from django.http import Http404
+from django.http import Http404, HttpResponse
 from django.shortcuts import get_object_or_404
 from django.core.paginator import Paginator
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView 
 from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 # Create your views here.
 def post_list(request):
@@ -34,13 +35,25 @@ def post_detail(request, year, month, day, post):
 
 def post_share(request, id):
     share_post= get_object_or_404(Post, pk= id, status = Post.Status.PUBLISHED)
+    sent = False
     if request.method=='POST':
         new_form= EmailPostForm(request.POST)
         if new_form.is_valid():
-            cd= new_form.cleaned_data #send email
+            print("DEBUG: Form is valid")
+            cd= new_form.cleaned_data #send email in a clean format
+            post_url= request.build_absolute_uri(share_post.get_absolute_url())
+            subject= (f"{cd['name']} ({cd['email']})" f"recommends you read post {share_post.title}")
+            message= (f"Read{share_post.title} at {post_url}\n\n" f"{cd['name']}\'s comments: {cd['comments']}")
+            send_mail(subject=subject, message=message, from_email=None, recipient_list=[cd['to']])
+            sent=True
+            return render(request, 'forms.html', {'share_post': share_post, 'new_form': new_form, 'sent':sent})
+
+        print("invalid")
+        print(new_form.errors)
+
     else:
         new_form= EmailPostForm()
-        return render(request, 'forms.html', {'share_post': share_post})
+        return render(request, 'forms.html', {'share_post': share_post, 'new_form': new_form, 'sent':sent})
 
             
 
