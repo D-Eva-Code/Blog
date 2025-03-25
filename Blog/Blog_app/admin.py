e88d6cf6809cf013eb0cc93ac25543465421d96f
Eva <anagwonyeadaeze42@gmail.com>
2025-03-25 17:14:10 +0100

    Registered model

--- Blog/Blog_app/admin.py
+++ Blog/Blog_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Post
+from.models import Comment
 
 # Register your models here.
 @admin.register(Post)
@@ -11,3 +12,10 @@ class PostAdmin(admin.ModelAdmin):
     ordering= ['status', 'publish']
     prepopulated_fields= {'slug': ('title',)}
     show_facets = admin.ShowFacets.ALWAYS
+
+@admin.register(Comment)
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display=['name', 'email','post','created', 'updated', 'active']
+    search_fields=['name', 'email', 'body']
+    list_filter=['created','updated', 'active']
